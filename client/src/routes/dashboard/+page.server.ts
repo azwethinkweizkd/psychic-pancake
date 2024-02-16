7699a5f3d4221d2052b2630250c8e16d115c961c
Kevin Devlin <azwethinkweizkd@gmail.com>
2024-02-15 20:36:34 -0700

    Completed moving things into server files to get correct url

--- client/src/routes/dashboard/+page.server.ts
+++ client/src/routes/dashboard/+page.server.ts
@@ -1,16 +1,9 @@
-import { getUserInfo } from '$lib/api/user/user';
 import type { PageServerLoad } from '../$types';
 
 export const load: PageServerLoad = async (event) => {
-	let { cookies, locals } = event;
-	let refreshToken = cookies.get('refreshToken');
-	let userInfoResponse = await getUserInfo(refreshToken);
-	let user = userInfoResponse?.user;
+	let { locals } = event;
 
-	if (user) {
-		locals.user = user;
-		return {
-			user: locals.user
-		};
-	}
+	return {
+		user: locals.user
+	};
 };
